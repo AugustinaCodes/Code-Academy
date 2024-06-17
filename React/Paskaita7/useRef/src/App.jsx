2d3c787d555df1ff60e79e7c754a0612b83a7866
August <76185140+Augustina1995@users.noreply.github.com>
2024-06-17 19:58:44 +0300

    lesson 7

---
+++ React/Paskaita7/useRef/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+import Timer from './components/Timer/Timer';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [showTimer, setshowTimer] = useState(true)
+
+  const persistentValue = useRef();
+
+  useEffect(() => {
+    // console.log("count changed", count);
+  }, [count])
+
+  useEffect(() => {
+    // console.log("tuscias useEffect array");
+
+    return () => {
+      // console.log("Cleanup");
+    }
+  }, [])
+
+  function handleClick() {
+    setCount((prev) =>  prev + 1)
+    setCount((prev) =>  prev + 1)
+  }
+
+  return (
+    <div>
+      <button onClick={() => persistentValue.current = 9}>Change useRef value</button>
+      <button onClick={() => console.log(persistentValue.current)}>Log persistentValue</button>
+      <button onClick={handleClick}>click me</button>
+      <h1>{count}</h1>
+      <button onClick={() => setshowTimer(!showTimer)}>Toggle Timer</button>
+      {
+        showTimer && <Timer />
+      }
+      <VideoPlayer />
+    </div>
+  )
+}
+
+export default App
---
+++ React/Paskaita7/useRef/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+
+export default function VideoPlayer() {
+
+    const videoPlayerRef = useRef()
+
+    // console.log(videoPlayerRef.current);
+
+  return (
+    <div>
+        <video ref={videoPlayerRef} src="/src/assets/videoplayback.mp4"></video>
+        <br />
+        <button onClick={() => videoPlayerRef.current.play()}>Play</button>
+        <button onClick={() => videoPlayerRef.current.pause()}>Pause</button>
+    </div>
+  )
+}
