cb6ce13ba0b7a5a2455ce8b61f0d352c9862235b
August <76185140+Augustina1995@users.noreply.github.com>
2024-07-15 19:30:50 +0300

    lesson23

---
+++ React/Paskaita23/forwardRef/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import Input from "./components/Input/Input";
+import { useRef } from "react";
+import RegisterSection from "./components/RegisterSection/RegisterSection";
+
+function App() {
+  const inputRef = useRef();
+  const registerSectionRef = useRef();
+
+  function handleFocusInput() {
+    inputRef.current.focus();
+  }
+
+  function handleScrollToSection() {
+    registerSectionRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  return (
+    <div>
+      <div style={{ height: "200vh" }}>
+        <RegisterSection ref={registerSectionRef} />
+        <Input ref={inputRef} value={"kazkas"} />
+        <button onClick={handleFocusInput}>Focus input</button>
+      </div>
+      <button onClick={handleScrollToSection}>
+        Leave your email for a newsletter
+      </button>
+    </div>
+  );
+}
+
+export default App;
