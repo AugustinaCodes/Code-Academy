11c8c1fc00c3662eaea8d11e9360f6c559d3ca33
August <76185140+Augustina1995@users.noreply.github.com>
2024-07-30 19:27:13 +0300

    useReducer and useContext

---
+++ React/Paskaita33/useReducerContext/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { ACTION_TYPES, useAppContext } from "../../context/AppContextProvider";
+
+export default function Header() {
+  const { dispatch } = useAppContext();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.DELETE_USER });
+        }}
+      >
+        Log out user
+      </button>
+    </div>
+  );
+}
